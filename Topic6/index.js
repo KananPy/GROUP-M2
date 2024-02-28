41c0db15b3413839870a0f5f1e4e9294b26c15ec
KananPy <kenan.semenderli@gmail.com>
2024-02-28 12:47:14 +0400

    Topic 6

---
+++ Topic6/index.js
@@ -0,0 +1,86 @@
+//String
+
+//NUmber  integer float
+
+//null
+
+//undefined
+
+//boolean
+
+var a = 'Javascript'
+
+// var b = 'Testing JS'
+
+// var a = 'Syntaxes'
+
+// console.log('Api data')
+
+// console.log('Next js', b);
+
+// var number1 = 5
+
+// var number2 = 10
+
+// var sum = 25
+
+// console.log('Numbers',number1, number2);
+
+// console.log(number1/number2);
+
+// var a = null
+
+// console.log(a);
+
+// var dogru = true
+
+// var yalnis = false
+
+// console.log(number1+number2 != sum);
+
+// var jsCode = 'React JS'
+
+// alert(jsCode)
+
+// var input = prompt('What is your name?')
+
+// console.log(input);
+
+// var tesdiq = confirm('Are you a programmer?')
+
+// console.log(tesdiq);
+
+
+var num1 = prompt('Number 1: ')
+var num2 = prompt('Number 2: ')
+
+// var numInt1 = parseInt(num1)
+
+// var numInt2 = parseInt(num2)
+
+// var sum = parseInt(num1) / parseInt(num2)
+
+alert(num1+num2)
+
+// console.log(sum);
+
+// var name = prompt('Adiniz: ')
+
+// var lastName = prompt('Soyadiniz: ')
+
+// alert(name + ' ' + '23'+ ' ' + lastName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
