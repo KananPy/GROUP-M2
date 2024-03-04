3780c00a52d864e68060aeaaf583e41bf51d7041
KananPy <kenan.semenderli@gmail.com>
2024-03-04 12:46:29 +0400

    array methods

---
+++ Topic6/array.js
@@ -0,0 +1,89 @@
+// array
+
+// var age = 23
+// var name = 'Ilqar'
+
+
+
+
+// var list = [23, 'Ilqar', true, 45, 'salam']
+
+
+
+// var mA = ['Salam', 'Nuru']
+
+// var mA2 = ['Test1', 'test2']
+
+// console.log('1ci list', list);
+
+// var del_list = list.pop()
+
+// var new_list = list
+
+// new_list.push('New el1', 'New el2', 45)
+
+// new_list.pop()
+
+// new_list.shift()
+
+// new_list.unshift('Nuru')
+
+// delete new_list[3]
+// console.log('2ci list', new_list);
+
+// var mergeArray = list.concat(mA, mA2)
+
+
+
+// console.log('merging arrays', mergeArray);
+
+// new_list.pop()
+
+
+
+// console.log('silinen', del_list);
+
+
+
+// console.log('Uzunluq', list.length);
+
+// console.log(list);
+// console.log(list.pop());
+// console.log(list);
+
+// if (list.length>3) {
+//     list.pop()
+//     list.push(5)
+//     console.log('Yeni list',list);
+// } else {
+//     console.log('uygun deyil');
+// }
+
+// console.log('shertden sonra uzunluq',list.length);
+
+// var list2 = ['Men', 'Junior', 'Developer-em' ]
+
+// // console.log(list2.join(' & '));
+
+// list2.push('yeni id1')
+
+// // list2.shift()
+
+
+// console.log(list2.join(' 1 '));
+
+
+var list = ['alma', 'armud', 'banan', 'nar']
+
+
+list.splice(2 , 0,  'Uzum')
+
+list.splice(2, 0, 'mango')
+
+var list_slice =  list.slice(4, 2)
+
+console.log('old list',list);
+
+console.log('slice', list_slice);
+
+
--- Topic6/index.js
+++ Topic6/index.js
@@ -4,11 +4,11 @@
 
 //null
 
-//undefined
+ //undefined
 
 //boolean
 
-var a = 'Javascript'
+// var a = 'Javascript'
 
 // var b = 'Testing JS'
 
@@ -51,8 +51,8 @@ var a = 'Javascript'
 // console.log(tesdiq);
 
 
-var num1 = prompt('Number 1: ')
-var num2 = prompt('Number 2: ')
+// var num1 = prompt('Number 1: ')
+// var num2 = prompt('Number 2: ')
 
 // var numInt1 = parseInt(num1)
 
@@ -60,7 +60,7 @@ var num2 = prompt('Number 2: ')
 
 // var sum = parseInt(num1) / parseInt(num2)
 
-alert(num1+num2)
+// alert(num1+num2)
 
 // console.log(sum);
 
@@ -69,6 +69,81 @@ alert(num1+num2)
 // var lastName = prompt('Soyadiniz: ')
 
 // alert(name + ' ' + '23'+ ' ' + lastName)
+
+// var name = ''
+
+// var pizzaName = ''
+
+// var pizzaPrice = 4
+
+// var pizzaCount = 3
+
+
+// 'Kanan has bought 3 pizzas, total cost is '
+
+// console.log(name, ' ', 'Aldim', pizzaName, '');
+
+// var num1 = 24
+
+// var num2 = 24
+
+// console.log(num1>=num2);
+
+// == , ===
+//!=, !==
+
+// var a = '5'
+
+// if(a!=5){
+//     console.log('Salam');
+// }else{
+//     console.log('Sert odemir');
+// }
+
+
+// var name = prompt('Adiniz: ')
+
+// var age = prompt('Yasiniz: ')
+
+// var locA = prompt('Sheher: ')
+
+// var ageInt = parseInt(age)
+
+// if(ageInt >= 80 && ageInt <=89){
+//     alert('A')
+// }else{
+//     alert('Melumat yalnisdir!')
+// }
+
+// var score = prompt('Your score: ')
+// var name = prompt('Your name')
+
+// if(score >= 90 && score <= 120){
+//     if(name == 'Telebe'){
+//         alert('Ela')
+
+//     }
+//     alert('A')
+// }else if(score >=80 && score <=89){
+//     alert('B')
+// }else if(score >=70 && score <= 79){
+//     alert('C')
+// }else if(score >=60 && score <= 69){
+//     alert('D')
+// }else{
+//     alert('F')
+// }
+
+var num = prompt('Your number: ')
+
+if(num % 4 == 0){
+    alert("Weldone!")
+}else{
+    alert('Think again')
+}
+
+
+
 
 
 
