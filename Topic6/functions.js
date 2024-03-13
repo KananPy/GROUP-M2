97d7e24544fc614bfd2cf783b367b56117a42dd4
KananPy <kenan.semenderli@gmail.com>
2024-03-13 12:28:56 +0400

    functions

---
+++ Topic6/functions.js
@@ -0,0 +1,72 @@
+"use strict"
+
+
+// var
+// let
+// const
+// const a = [1,4,6]
+// a.push(435)
+// a.pop()
+
+// if(true){
+   
+//     console.log(a);
+
+// }
+
+// console.log(a);
+// let arr1 = [2,5,78,8,54,3]
+
+// for (let i of arr1) {
+//     console.log(i);
+    
+// }
+
+// function add(a){
+//     for(let i of a){
+//         console.log(i);
+//     }
+// }
+
+// let arr1 = [2,5,78,8,54,3]
+// let arr2 = [2,5,8,3243,34,4756]
+// let arr3 = [8,0,9,76,'sasdas',54]
+
+// add(arr1)
+// add(arr2)
+
+// let num1 = 4
+// let num2 = 9
+
+// function sum(a,b){
+
+//     let add = a + b 
+
+//      return add
+// }
+
+// console.log(sum(5,7));
+
+let a = -745
+
+console.log(a.toString().length-1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Topic6/loop.js
+++ Topic6/loop.js
@@ -1,21 +1,231 @@
-var text = 'I love coding as I love you'
 
-// var num = 43632
 
-var arr = text.split(' ')
+let x = "\10"
 
-console.log(arr);
+console.log(x);
+// var text = 'I love coding as I love you'
 
-var arr2 = []
+// // var num = 43632
 
-// var numbers = num.toString()
-for(var i = 0; i < arr.length; i++){
-    if (arr[i].includes('o')) {
-        arr2.push(arr[i])
-    } 
+// var arr = text.split(' ')
 
-    console.log(arr[i]);
+// console.log(arr);
+
+// var arr2 = []
+
+// // var numbers = num.toString()
+// for(var i = 0; i < arr.length; i++){
+//     if (arr[i].includes('o')) {
+//         arr2.push(arr[i])
+//     }
+
+//     console.log(arr[i]);
+
+// }
+
+// console.log(arr2);
+
+// tapsiriq 9
+
+// for(let i = 100; i <= 999; i++){
+//     let digit1 = parseInt(i / 100);
+//     let digit2 = parseInt((i % 100) / 10);
+//     let digit3 = i % 10;
+
+//     if (i === (digit1 ** 3 + digit2 ** 3 + digit3 ** 3)) {
+//         console.log(`${i} is amrstrong number`);
+//     }
+// }
+
+//tapsiriq 8
+
+// var text = "Write a JavaScript program that displays the largest integer among two integers";
+
+// var arr = text.split(' ');
+
+// var a = ''
+
+// var arr2 = []
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - i - 1; j++) {
+//         // Compare the lengths of adjacent words
+//         if (arr[j].length > arr[j + 1].length) {
+//             // Swap the elements
+//             console.log(arr[j+1]);
+//             // let temp = arr[j];
+//             // arr[j] = arr[j + 1];
+//             // arr[j + 1] = temp;
+//         }
+//     }
+
+// }
+
+// console.log(arr2);
+
+// var a = 4
+// console.log(a);
+// a = a + 5
+
+// console.log(a);
+
+// a += 10
+
+// for (var i = 0; i < text.length; i++) {
+//     // console.log(text[i]);
+//   if (text[i] === "a") {
+//     new_text = new_text + "x";
+//   } else {
+//     new_text = new_text + text[i];
+//     console.log(new_text);
+//   }
+// }
+
+// console.log(text);
+// console.log(new_text);
+
+// var arr = [13, 23, 12, 45, 22];
+// console.log(arr);
+
+// for (var i = 0; i < arr.length; i++) {
+//     if(i=== 3){
+//       continue 
+//     }
+//     console.log(arr[i]);
+  
+// }
+
+// console.log(arr);
+
+
+// var n = 0
+
+// while(n < 10){
+  
+//   n++
+//   console.log('while dovr',n);
+// }
+
+// for(var i = 0; i < 10; i++){
+//   console.log('for dovr',i);
+// }
+
+
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr);
+//     console.log(i);
+//   if (arr[i] % 2 === 0) {
+//     arr.splice(i, 1);
+//   }
+// }
+// console.log(arr);
+
+// var i = 0
+
+// while (i< arr.length-1){
+//   i++
+//     console.log(arr[i]);
+// }
+
+
+// var arr = [['Game', 'developer'],[3, 4], [5, 6]]
+
+// console.log(arr[1][1]);
+
+// var new_arr = []
+
+
+
+
+
+// for(var i = 0;i<arr.length;i++){
+//   for(var j = 0; j<arr[i].length;j++){
+
+//     console.log(arr[i][j]);
+//   }
     
-}
+  
+// }
 
-console.log(arr2);
+// var a = 435389
+
+// var num = a.toString()
+
+// var sum = 0
+
+// for(var i = 0; i < num.length; i++){
+//   sum+= parseInt(num[i])
+// }
+
+// console.log(sum);
+
+// var arr = [2, 45, 3, 67, 34, 567, 34, 345, 123] 
+
+// var max_num = arr[0]
+
+// for (let index = 0; index < arr.length; index++) {
+//   if(arr[i]> max_num){
+//     max_num=arr[i]
+//   }
+  
+// }
+
+// console.log(max_num);
+
+// let arr = [6, 2, 1, 8, 10];
+
+// console.log(arr.join(''));
+
+// console.log(boyukTogrul.toString(' '));
+
+// let max = arr[0];
+// let min = arr[0];
+
+
+// for(let i=0; i< arr.length;i++){
+  
+//   if(max< arr[i]){
+//     max = arr[i]
+    
+//   }
+//   if(min> arr[i]){
+//     min = arr[i]
+    
+//   }
+//  }
+//  console.log(max);
+//  console.log(min);
+// let minNumber = arr[0];
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   if (arr[i] > maxNumber) {
+//     maxNumber = arr[i];
+//     arr.splice(i, 1);
+//   } else if (arr[i] < minNumber) {
+//     minNumber = arr[i];
+//     arr.splice(i, 1);
+//   }
+// }
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// console.log(sum);
+
+// var arr =[20,-1,-7,8,0.12,7,8]
+
+// const person = {
+//     fname:"John",
+//     lname:"Doe",
+//     age:25
+//   }; 
+// person.nationality = 'Turk'
+
+//   console.log(person);
+
+//   for (var index = 0; index < 6; index++) {
+   
+    
+//   }
+
+//   console.log(index);
